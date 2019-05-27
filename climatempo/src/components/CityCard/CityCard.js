0ac4136dced684abb6c51cd035b78be29962dd6a
eron c <deandradeeron@gmail.com>
2019-05-27 18:57:58 -0300

    Box design

--- climatempo/src/components/CityCard/CityCard.js
+++ climatempo/src/components/CityCard/CityCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import  '../CityCard/CityCard.css';
+import classes from '../CityCard/CityCard.css';
 
 class CityCard extends Component {
 
@@ -7,8 +7,25 @@ class CityCard extends Component {
 
     render() {
         return (
-                <div className="CardContainer">
-                   Placeholder
+                <div className={classes.CardContainer}>                 {/* A caixa externa */}
+                    <div className={classes.CardHeaderDate}>            {/* Faixa superior com a data */}
+                        27/05/2019
+                    </div>
+                    <div className={classes.CardWeatherDescription}>    {/* Segunda faixa, com descrições do clima*/}
+                        Céu claro com núvens sendo portanto chamado parcialmente nublado.
+                    </div>
+                    <div className={classes.WeatherNumbers}>            {/* Caixa das duas faixas inferiores*/}
+                        <div className={classes.CardRows}>              {/* Faixa de temperaturas máxima e mínima*/}
+                            <div className={classes.Box1}><img src={ require('../../assets/images/arrowup.JPG') }/></div>
+                            <div className={classes.Box2}>20°C</div>
+                            <div className={classes.Box1}><img src={ require('../../assets/images/arrowdn.JPG') }/></div>
+                            <div className={classes.Box3}>15°C</div>
+                        </div>
+                        <div className={classes.CardRows}>              {/* Faixa inferior: chance de chuva e precipitação*/}
+                            Placeholder
+                        </div>
+
+                    </div>
                 </div>
         );
     }
