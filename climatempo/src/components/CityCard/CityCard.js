83b5cdf30678b36d15524d4df7f9c100868a318b
eron c <deandradeeron@gmail.com>
2019-05-27 16:19:29 -0300

    Commit to eject

---
+++ climatempo/src/components/CityCard/CityCard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import  '../CityCard/CityCard.css';
+
+class CityCard extends Component {
+
+
+
+    render() {
+        return (
+                <div className="CardContainer">
+                   Placeholder
+                </div>
+        );
+    }
+};
+
+export default CityCard;
