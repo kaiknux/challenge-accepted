c4792c0d1f2b3f19fe5bd52ae048de667855a4dc
eron c <deandradeeron@gmail.com>
2019-06-02 14:37:46 -0300

    Random Commit

--- climatempo/src/components/CityCard/CityCard.js
+++ climatempo/src/components/CityCard/CityCard.js
@@ -6,23 +6,23 @@ function CityCard (props) {
         return (
                 <div className={classes.CardContainer}>                 {/* A caixa externa */}
                     <div className={classes.CardHeaderDate}>            {/* Faixa superior com a data */}
-                         
+                         {props.date}
                     </div>
                     <div className={classes.CardWeatherDescription}>    {/* Segunda faixa, com descrições do clima*/}
-                        Céu claro com núvens sendo portanto chamado parcialmente nublado.
+                        {props.text}
                     </div>
                     <div className={classes.WeatherNumbers}>            {/* Caixa das duas faixas inferiores*/}
                         <div className={classes.CardRows}>              {/* Faixa de temperaturas máxima e mínima*/}
                             <div className={classes.Box1}><img src={ require('../../assets/images/arrowup.JPG') }/></div>
-                            <div className={classes.Box2}>20°C</div>
+                            <div className={classes.Box2}>{props.tempmax}</div>
                             <div className={classes.Box1}><img src={ require('../../assets/images/arrowdn.JPG') }/></div>
-                            <div className={classes.Box3}>15°C</div>
+                            <div className={classes.Box3}>{props.tempmin}</div>
                         </div>
                         <div className={classes.CardRows}>              {/* Faixa inferior: chance de chuva e precipitação*/}
                             <div className={classes.Box1}><img src={ require('../../assets/images/arrowup.JPG') }/></div>
-                            <div className={classes.Box4}>10mm</div>
+                            <div className={classes.Box4}>{props.rainprec}</div>
                             <div className={classes.Box1}><img src={ require('../../assets/images/arrowdn.JPG') }/></div>
-                            <div className={classes.Box4}>40%</div>
+                            <div className={classes.Box4}>{props.rainprob}</div>
                         </div>
 
                     </div>
--- climatempo/src/containers/AppBlock/AppBlock.js
+++ climatempo/src/containers/AppBlock/AppBlock.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import CityCard from '../../components/CityCard/CityCard';
 
-const axios = require('axios'); // not using it rn
-
 class AppBlock extends Component {
 
- state = {
-    date: '',
-    text: '',
-    maxtemperature: '',
-    mintemperature: '',
-    rainprobability: '',
-    rainprecipitation: '',
-    };
- 
+ state = {  cityOneInfo: '',
+            cityTwoInfo: '',
+            cityOneWeekWeather: '',
+            cityTwoWeekWeather: '',
+            todayDate: '',
+            todayText: '',
+            minTemperature: '',
+            maxTemperature: '',
+            rainProbability: '',
+            rainPrecipitation: '',
+            appear: false,
+}
     searchData = async () => { 
         // API call
         let dataLocale = await fetch('https://api.myjson.com/bins/d0et7');
@@ -25,33 +26,74 @@ class AppBlock extends Component {
 
     parseChecker = (resLocale, resWeather) => {
         this.setState()
-                                                                               console.log(resLocale);
-                                                                               console.log(resWeather);
         if (resLocale !== undefined && resWeather !== undefined) {
             this.updateFromServer(resWeather)
         } else { console.log('error fetchcing data')}
 
     };
 
-    arrayOrganize = (resWeather) => {
+    organizeWeekWeather = (resWeather) => {
         let i = '';
         for (i = 0; i < resWeather.length; i++) {
             return resWeather[i].weather;
         }
     }
 
+    cityInfoEngine = (resWeather) => {
+        const locales = resWeather.map(city => city.locale)
+        return locales;
+    };
+
     updateFromServer = (resWeather) => {
         let dataHandled = [...resWeather];
         const weekDaysWeather = dataHandled.map((x) => {
-            return this.arrayOrganize(dataHandled)
+            return this.organizeWeekWeather(dataHandled)
         });
-        console.log(weekDaysWeather);
+        const cityInfo = this.cityInfoEngine(resWeather);
+        const cityOneWeekWeather = weekDaysWeather[0];
+        const cityTwoWeekWeather = weekDaysWeather[1];
+        const cityOneInfo = cityInfo[0];
+        const cityTwoInfo = cityInfo[1];
+        this.stateChangeHandler(cityOneWeekWeather, cityTwoWeekWeather, cityOneInfo, cityTwoInfo);
+        // this.setState({cityOneInfo: cityOneInfo})
+        // this.setState({cityTwoInfo: cityTwoInfo})
+        this.setState({cityOneWeekWeather: cityOneWeekWeather})
+        // this.setState({cityTwoWeekWeather: cityTwoWeekWeather})
+                                                                                console.log('state updated:');
+                                                                                console.log(this.state);
+    };
+
+    stateChangeHandler = (cityOneWeekWeather, cityTwoWeekWeather, cityOneInfo, cityTwoInfo) => {
+        const todayDate = cityOneWeekWeather[0].date;
+        this.setState({todayDate: todayDate});
+        const todayText = cityOneWeekWeather[0].text;
+        this.setState({todayText: todayText});
+        const minTemperature = cityOneWeekWeather[0].temperature.min;
+        this.setState({minTemperature: minTemperature});
+        const maxTemperature = cityOneWeekWeather[0].temperature.max;
+        this.setState({maxTemperature: maxTemperature});
+        const rainProbability = cityOneWeekWeather[0].rain.probability;
+        this.setState({rainProbability: rainProbability});
+        const rainPrecipitation = cityOneWeekWeather[0].rain.precipitation;
+        this.setState({rainPrecipitation: rainPrecipitation});
+        // second box item
+        const todayDate1a = cityOneWeekWeather[1].date;
+        this.setState({todayDate1a: todayDate1a});
+        const todayText1a = cityOneWeekWeather[1].text;
+        this.setState({todayText1a: todayText1a});
+        const minTemperature1a = cityOneWeekWeather[1].temperature.min;
+        this.setState({minTemperature1a: minTemperature1a});
+        const maxTemperature1a = cityOneWeekWeather[1].temperature.max;
+        this.setState({maxTemperature1a: maxTemperature1a});
+        const rainProbability1a = cityOneWeekWeather[1].rain.probability;
+        this.setState({rainProbability1a: rainProbability1a});
+        const rainPrecipitation1a = cityOneWeekWeather[1].rain.precipitation;
+        this.setState({rainPrecipitation1a: rainPrecipitation1a});
+        this.setState({appear: true})
+                                                                                console.log(this.state);
+
     };
         
-
-
-        // [{chuloo: "grilled chicken", age: 60}, {chris: "cold beer", age: 50}, {sam: "fish biscuits", age: 30}]
-
         // obj[i].text = resWeather[i].weather[0].text,
         // obj[i].maxtemperature = resWeather[i].weather[0].temperature.max,
         // obj[i].mintemperature = resWeather[i].weather[0].temperature.min,
@@ -59,15 +101,30 @@ class AppBlock extends Component {
         // obj[i].rainprecipitation = resWeather[i].weather[0].rain.precipitation,
 
 
-    render() {
+    render() 
+        let jsx =  <div>
+            <CityCard date={this.state.todayDate}
+                text={this.state.todayText}
+                tempmax={this.state.maxTemperature}
+                tempmin={this.state.minTemperature}
+                rainprob={this.state.rainProbability}
+                rainprec={this.state.rainPrecipitation}
+            />
+            <CityCard date={this.state.todayDate1a}
+                text={this.state.todayText1a}
+                tempmax={this.state.maxTemperature1a}
+                tempmin={this.state.minTemperature1a}
+                rainprob={this.state.rainProbability1a}
+                rainprec={this.state.rainPrecipitation1a}
+            />
+        </div>;
 
     return (
         <div>
-            
-        <CityCard/>
-        <CityCard/>
+        {this.showUp}
         <button onClick={this.searchData}>Check</button>
         </div>
+        };
     );
 }};
 
